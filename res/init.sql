0f2e75da9d282d0ccd3356ee47e7669d443b7e78
Hepheir <hepheir@gmail.com>
2026-06-12 10:16:08 +0900

    chore: ssafy_ai 데이터베이스 추가

--- res/init.sql
+++ res/init.sql
@@ -1,13 +1,13 @@
 -- 추가 데이터베이스 생성
-CREATE DATABASE IF NOT EXISTS ssafydb;
-CREATE DATABASE IF NOT EXISTS ssafylive;
+CREATE DATABASE IF NOT EXISTS ssafydb DEFAULT CHARACTER SET utf8mb4;
+CREATE DATABASE IF NOT EXISTS ssafylive DEFAULT CHARACTER SET utf8mb4;
+CREATE DATABASE IF NOT EXISTS ssafy_ai DEFAULT CHARACTER SET utf8mb4;
 
 -- 새로운 사용자 생성 및 비밀번호 설정 (MySQL 8.0 이상 기준)
 CREATE USER 'ssafy'@'%' IDENTIFIED BY 'ssafy';
 
 -- 생성된 모든 데이터베이스에 대한 권한 부여
-GRANT ALL PRIVILEGES ON ssafydb.* TO 'ssafy'@'%';
-GRANT ALL PRIVILEGES ON ssafylive.* TO 'ssafy'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'ssafy'@'%';
 
 -- 권한 적용
 FLUSH PRIVILEGES;
